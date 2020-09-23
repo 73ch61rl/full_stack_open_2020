ad74a02d5b470391d12876c2ad5b48e8ca711f30
Irina Koski <irina.smirnova86@gmail.com>
2020-09-23 13:19:52 +0300

    Middle version before changing to array, 1.4: course information, step 4

--- part_1/courseinfo/src/index.js
+++ part_1/courseinfo/src/index.js
@@ -29,30 +29,32 @@ const Total = props => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
       <Content 
-        partName1={part1.name}
-        exNumber1={part1.exercises}
-        partName2={part2.name}
-        exNumber2={part2.exercises}
-        partName3={part3.name}
-        exNumber3={part3.exercises}/>
-      <Total number1={part1.exercises} number2={part2.exercises} number3={part3.exercises}/>
+        partName1={parts[0].name}
+        exNumber1={parts[0].exercises}
+        partName2={parts[1].name}
+        exNumber2={parts[1].exercises}
+        partName3={parts[2].name}
+        exNumber3={parts[2].exercises}/>
+      <Total number1={parts[0].exercises} number2={parts[1].exercises} number3={parts[2].exercises}/>
     </div>
   )
 }
