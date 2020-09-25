a43f7eb19482a2cb412506a191b82dcb4bcfd027
Irina Koski <irina.smirnova86@gmail.com>
2020-09-25 23:12:56 +0300

    1.7: unicafe step 2

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -9,6 +9,9 @@ const Stats = props => {
       <Stat name="good" value={props.good}/>
       <Stat name="bad" value={props.bad}/>
       <Stat name="neutral" value={props.neutral}/>
+      <Stat name="all" value={props.all}/>
+      <Stat name="average" value={props.avg}/>
+      <Stat name="positive" value={props.positive}/>
     </div>
   );
 };
@@ -18,6 +21,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
+  const all = good + bad + neutral;
+  const avg = ((good * 1) + (bad * (-1)) + (neutral * 0)) / all
+  const positive = (good * 100) / all
 
   const setFeedback = (param) => {
     switch (param) {
@@ -35,12 +41,12 @@ const App = () => {
 
   return (
     <div>
-      <Header text="give feedback" />
-      <Button onClick={() => setFeedback("good")} text="good" />
-      <Button onClick={() => setFeedback("bad")} text="bad" />
-      <Button onClick={() => setFeedback("neutral")} text="neutral" />
-      <Header text="statistics" />
-      <Stats good={good} bad={bad} neutral={neutral}/>
+      <Header text="give feedback"/>
+      <Button onClick={() => setFeedback("good")} text="good"/>
+      <Button onClick={() => setFeedback("bad")} text="bad"/>
+      <Button onClick={() => setFeedback("neutral")} text="neutral"/>
+      <Header text="statistics"/>
+      <Stats good={good} bad={bad} neutral={neutral} all={all} avg={avg} positive={positive}/>
     </div>
   );
 };
