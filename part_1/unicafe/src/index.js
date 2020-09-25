70e371174cd5f76ec231bdc2545bf94cacc0372f
Irina Koski <irina.smirnova86@gmail.com>
2020-09-25 16:26:55 +0300

    1.6: unicafe step 1

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -1,19 +1,48 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = props => <h1>{props.text}</h1>;
+const Button = props => <button onClick={props.onClick}>{props.text}</button>;
+const Stats = props => {
+  return (
+    <div>
+      <Stat name="good" value={props.good}/>
+      <Stat name="bad" value={props.bad}/>
+      <Stat name="neutral" value={props.neutral}/>
+    </div>
+  );
+};
+const Stat = props => <div>{props.name} {props.value}</div>
+
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const setFeedback = (param) => {
+    switch (param) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+    }
+  };
 
   return (
     <div>
-      code here
+      <Header text="give feedback" />
+      <Button onClick={() => setFeedback("good")} text="good" />
+      <Button onClick={() => setFeedback("bad")} text="bad" />
+      <Button onClick={() => setFeedback("neutral")} text="neutral" />
+      <Header text="statistics" />
+      <Stats good={good} bad={bad} neutral={neutral}/>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
